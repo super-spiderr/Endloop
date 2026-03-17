a345441fdb662bcea8484b52dad752643b498248
Vignesh <vigneshbalasubramaniyan1409@gmail.com>
2026-03-17 16:06:32 +0530

    package update

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-
 export const Login = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Login</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Login screen</Text>
+    </View>
+  );
+};
